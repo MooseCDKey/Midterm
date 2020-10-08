5949a54e5828873bfab1534aa87d7c69f6efd114
Mustafa Siddiqi <mustafasiddiqi00@gmail.com>
2020-10-08 17:34:41 -0400

    Unity Project V1
    Unity Project, Movement and Scene Manager

---
+++ Midterm/Assets/Scripts/FPSLook.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSLook : MonoBehaviour
+{
+    public float mouseSense = 200f;
+
+    public Transform playerBody;
+
+    float rotationX = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSense * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSense * Time.deltaTime;
+
+        rotationX -= mouseY;
+
+        rotationX = Mathf.Clamp(rotationX, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(rotationX, 0f, 0f);
+
+     
+
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ Midterm/Assets/Scripts/SceneManage.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManage : MonoBehaviour
+{
+    public void PlayG()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitG()
+    {
+        Application.Quit();
+    }
+
+    public void PlaySFromMenu()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+
+    public void PlaySAfterG()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); //used after maze is done
+    }
+
+    public void ReturnToM()//used on statistics scene
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
+    }
+
+}
